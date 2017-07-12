fc6e45e2356dba9732cea9967cd0c3d838b385bd
rusher <diego.dupin@gmail.com>
2017-07-12 19:43:20 +0200

    corrected maxscale version

--- .travis/script.sh
+++ .travis/script.sh
@@ -18,13 +18,14 @@ for i in {30..0}; do
     sleep 1
 done
 
+docker-compose -f .travis/docker-compose.yml logs
+
 if [ "$i" = 0 ]; then
     echo 'SELECT 1' | "${mysql[@]}"
     echo >&2 'Maxscale init process failed.'
     exit 1
 fi
 
-docker-compose -f .travis/docker-compose.yml logs
 
 ###################################################################################################################
 # run test suite
