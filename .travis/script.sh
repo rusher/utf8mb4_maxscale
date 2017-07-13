9337a14adfd40ea34be6e957aae03775d235c199
rusher <diego.dupin@gmail.com>
2017-07-13 11:27:24 +0200

    corrected maxscale version

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,7 +8,7 @@ cmd=( mvn clean test )
 docker-compose -f .travis/docker-compose.yml build
 docker-compose -f .travis/docker-compose.yml up -d
 
-mysql=( mysql --protocol=tcp -ubob -h127.0.0.1 --port=4007 )
+mysql=( mysql --protocol=tcp -ubob -h127.0.0.1 --port=4006 )
 
 for i in {30..0}; do
     if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
